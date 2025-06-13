4a43a4226b62f66689d75ea1f4dada39121ea939
Abhinav Bajpai <74667440+abhinavbajpayee@users.noreply.github.com>
2025-06-13 06:24:56 +0000

    conversion and datatype is done

---
+++ 01_basis/datatypesECMA.js
@@ -0,0 +1,17 @@
+"use strict";//treat all JS code as newer version
+// alert(3+3)will give error as we are using node.js not brwoser
+/*console.log(3
+        +3)here we find that code readinilty is not perfect*/
+// ecma very useful
+
+// number=> range: 2^53
+// bigint
+// boolean=>true/false
+// null=> standalone value
+// undefined=> values not assigned
+// symbol=>unique
+// object
+// we use tyoeof to find the type of datatype
+// null is a datatype:OBJECT
+
+// mdn doucmentation
