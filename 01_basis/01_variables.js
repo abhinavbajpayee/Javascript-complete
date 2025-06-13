177e5faedf6e438702899e9289b0aa9870aa14d3
Abhinav Bajpai <74667440+abhinavbajpayee@users.noreply.github.com>
2025-06-13 05:49:02 +0000

    var done

---
+++ 01_basis/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail= "abhinav@google.com"
+var accountPassword="12345"
+accountCity="Lucknow"
+
+// accountId=2 not possible as it is in const
+
+
+// console.log(accountId);
+console.table([accountEmail, accountId,accountCity,accountPassword])
