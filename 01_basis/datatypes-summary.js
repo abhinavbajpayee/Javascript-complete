950b659903e462c49269c58c7c69caa693deece8
Abhinav Bajpai <74667440+abhinavbajpayee@users.noreply.github.com>
2025-06-13 10:35:14 +0000

    datatype summary

--- 01_basis/datatypes-summary.js
+++ 01_basis/datatypes-summary.js
@@ -14,16 +14,17 @@ const anotherId = Symbol('123')
 
 console.log(id === anotherId);
 
-// const bigNumber = 3456543576654356754n
-
+const bigNumber = 3456543576654356754n
+// here we have to use n at last
 
 
 // Reference (Non primitive)
-
+// these data type work on call by reference;i.e these are the onces when we call them we get thier location rather than value
 // Array, Objects, Functions
 // JS is dynamically typed as in this variable type is defined at runtime not compile time
 // let A="hero" ; this will make var A as string automatically even though we have not defined it before
 const heros = ["shaktiman", "naagraj", "doga"];
+
 let myObj = {
     name: "hitesh",
     age: 22,
@@ -35,4 +36,19 @@ const myFunction = function(){
 
 console.log(typeof anotherId);
 
+// for all non-primitive datatypes we get result as funtion but for function we get as object
+
+/*//primitive typeof values : 
+// Number => number
+// String => string
+// Boolean => boolean
+// null => object
+// Undefined => undefined
+// Symbol => symbol
+// BigInt => bigint
+
+//for non prem 
+// array => object.
+// object => object.
+// function => function. // said as : (function object)*/
 // https://262.ecma-international.org/5.1/#sec-11.4.3
